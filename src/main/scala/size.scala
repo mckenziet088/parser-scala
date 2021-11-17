86ac785191b2f40d003b297a15b4e6ac6746e6fb
mckenziet088 <91046902+mckenziet088@users.noreply.github.com>
2021-11-17 15:51:00 -0600

    Create size.scala

---
+++ src/main/scala/size.scala
@@ -0,0 +1,13 @@
+package nanol
+
+/**
+ * Counts the number of atoms in a nanoL expression.
+ */
+object size {
+
+  import NanoElement.*
+
+  // TODO implement this behavior
+
+  def apply(e: NanoElement): Int = ???
+}
